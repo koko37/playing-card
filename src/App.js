43e2edc2337396f4b85ba1083f506a049c72e859
wjone <engel0088@gmail.com>
2020-07-23 11:11:04 +0900

    feat: restart game

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { connect } from 'react-redux'
 import { Container, Jumbotron, Row, Col, Button  } from 'react-bootstrap';
 import CardHolder from "./components/CardHolder"
@@ -23,13 +23,9 @@ const App = ({score, centerHoldersStatus, resetPickCard, resetHolder}) => {
         width: "100px", 
         height: "120px"
     }
-
-    const cardData = new Array(15);
-    var fistRowOfHolders = new Array(0);
-    var lastRowOfHolders = new Array(0);
-    var centerRowOfHolders = new Array(0);
-
+    
     const initCardData = () => {
+      var cardData = new Array(16);
       let cardDataInit = [];
       let n, firstId, secondId;
       let holderNo;
@@ -54,27 +50,27 @@ const App = ({score, centerHoldersStatus, resetPickCard, resetHolder}) => {
         }
         cardData[holderNo] = holder_card_data;
       }
+      
+      // keep residue cards into final holder
+      const spare_card_holder_data = new Array(7);
+      n = 0;
+      while(firstId < 52)
+      {
+        spare_card_holder_data[n++] = getCardFromNumber(cardDataInit[firstId++]);
+      }
+      cardData[15] = spare_card_holder_data;
+      console.log("log card: ", cardData[0]);
+      return cardData;
+    }
 
-      // prepare card holder rows
-      for (n=0; n<5; n++)
-      {
-        fistRowOfHolders.push(<CardHolder cards={cardData[n]} card_size={card_size} id={n} key={n}/>)
-      }
-      for (n=5; n<10; n++)
-      {
-        lastRowOfHolders.push(<CardHolder cards={cardData[n]} card_size={card_size} id={n} key={n}/>)
-      }
-      for (n=10; n<15; n++)
-      {
-        centerRowOfHolders.push(<CardHolder cards={cardData[n]} card_size={card_size} id={n} key={n} disable={centerHoldersStatus[n-10]} />)
-      }
-
-      // reset store status
+    const onClickRestart = () => {
+      console.log("Restart game.");
+      setHolderData(initCardData());
       resetPickCard();
       resetHolder();
     }
-
-    initCardData();
+    
+    const [holderData, setHolderData] = useState(initCardData());
 
     return (
         <Container>
@@ -82,7 +78,7 @@ const App = ({score, centerHoldersStatus, resetPickCard, resetHolder}) => {
                 <div className="d-flex justify-content-between align-items-center">
                   <h1 className="text-primary">Playing card game</h1>
 
-                  <Button variant="danger">Restart</Button>
+                  <Button variant="danger" onClick={() => onClickRestart()}>Restart</Button>
                 </div>
                 <Row>
                   <Col md="10" sm="10" lg="10">
@@ -97,18 +93,33 @@ const App = ({score, centerHoldersStatus, resetPickCard, resetHolder}) => {
                 
                 <Row>
                     <Col className="d-flex justify-content-between mb-2 px-5">
-                      { fistRowOfHolders }
+                      <CardHolder cards={holderData[0]} card_size={card_size} id={0} key={0}/>
+                      <CardHolder cards={holderData[1]} card_size={card_size} id={1} key={1}/>
+                      <CardHolder cards={holderData[2]} card_size={card_size} id={2} key={2}/>
+                      <CardHolder cards={holderData[3]} card_size={card_size} id={3} key={3}/>
+                      <CardHolder cards={holderData[4]} card_size={card_size} id={4} key={4}/>
                     </Col>
                 </Row>
                 <Row>
                     <Col className="d-flex justify-content-between mb-2 px-5">
-                    { centerRowOfHolders }
+                      <CardHolder cards={holderData[10]} card_size={card_size} id={10} key={10} disable={centerHoldersStatus[0]} />
+                      <CardHolder cards={holderData[11]} card_size={card_size} id={11} key={11} disable={centerHoldersStatus[1]} />
+                      <CardHolder cards={holderData[12]} card_size={card_size} id={12} key={12} disable={centerHoldersStatus[2]} />
+                      <CardHolder cards={holderData[13]} card_size={card_size} id={13} key={13} disable={centerHoldersStatus[3]} />
+                      <CardHolder cards={holderData[14]} card_size={card_size} id={14} key={14} disable={centerHoldersStatus[4]} />
                     </Col>
                 </Row>
                 <Row>
                     <Col className="d-flex justify-content-between mb-2 px-5">
-                    { lastRowOfHolders }
+                      <CardHolder cards={holderData[5]} card_size={card_size} id={5} key={5}/>
+                      <CardHolder cards={holderData[6]} card_size={card_size} id={6} key={6}/>
+                      <CardHolder cards={holderData[7]} card_size={card_size} id={7} key={7}/>
+                      <CardHolder cards={holderData[8]} card_size={card_size} id={8} key={8}/>
+                      <CardHolder cards={holderData[9]} card_size={card_size} id={9} key={9}/>
                     </Col>
+                </Row>
+                <Row>
+                  <CardHolder cards={holderData[15]} card_size={card_size} id={15} key={15} />
                 </Row>
             </Jumbotron>
         </Container>
--- src/reducers/scoreReducer.js
+++ src/reducers/scoreReducer.js
@@ -7,21 +7,27 @@ export const initialState = {
 }
 
 export default function scoreReducer(state = initialState, action) {
+  var newCenterRowsState = [];
+  var blankHoldersNew = [];
   switch(action.type) {
     case actions.EMPTY_HOLDER:
-      state.blankHolders.push(action.payload);
-      // check whether pair holder is already empty?
-      var pairId = state.blankHolders.indexOf( (action.payload < 5) ? (action.payload+5) : (action.payload-5));
+      console.log("empty_holder: ", action.payload);
+      Object.assign(blankHoldersNew, state.blankHolders);
+      Object.assign(newCenterRowsState, state.centerRowsDisableState);
       var newScore = state.score + ((action.payload < 10) ? 1000 : 10000);
+
+      blankHoldersNew.push(action.payload);
+      // check whether pair holder is already empty?
+      var pairId = blankHoldersNew.indexOf( (action.payload < 5) ? (action.payload+5) : (action.payload-5));
       // if two pair holder are opened already, then open the center holder.
       if(pairId !== -1)
       {
-        state.centerRowsDisableState[action.payload % 5] = false;
+        newCenterRowsState[action.payload % 5] = false;
       }
       return {
         score: newScore,
-        centerRowsDisableState: state.centerRowsDisableState,
-        blankHolders: state.blankHolders
+        centerRowsDisableState: newCenterRowsState,
+        blankHolders: blankHoldersNew
       }
 
     case actions.RESET_HOLDERS:
