056c9eaf085181d1f435de00286f6b62b40deff8
wjone <engel0088@gmail.com>
2020-08-01 15:21:31 +0900

    Updates

---
+++ precache-manifest.2e49299a374a4d6c383aad5a607c73fc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5129c2250e12743a2da30f2e71ef728",
+    "url": "/playing-card/index.html"
+  },
+  {
+    "revision": "9e4578e6194d72bea9a5",
+    "url": "/playing-card/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "43b70e247bedeb19d53a",
+    "url": "/playing-card/static/css/main.144ed686.chunk.css"
+  },
+  {
+    "revision": "9e4578e6194d72bea9a5",
+    "url": "/playing-card/static/js/2.f8bbdfc3.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/playing-card/static/js/2.f8bbdfc3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43b70e247bedeb19d53a",
+    "url": "/playing-card/static/js/main.0fa8a8ec.chunk.js"
+  },
+  {
+    "revision": "d2df80a6e1c559d90c81",
+    "url": "/playing-card/static/js/runtime-main.c2ffcd2c.js"
+  }
+]);
--- src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux'
-import scoreReducer from './scoreReducer'
-
-const rootReducer = combineReducers({
-  score: scoreReducer
-})
-
-export default rootReducer;
