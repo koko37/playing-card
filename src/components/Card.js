27b65f2650e82e7bed77051fb087ed5bd5232b8e
wjone <engel0088@gmail.com>
2020-08-01 16:53:33 +0900

    refactor: lint error

--- src/components/Card.js
+++ src/components/Card.js
@@ -7,39 +7,39 @@ import heart from "../imgs/h.png"
 import spade from "../imgs/s.png"
 
 export default ({id, card, size, active, pickup}) => {
-    const width = size != null ? size.width : "120px"
-    const height = size != null ? size.height : "150px"
+  const width = size != null ? size.width : "120px"
+  const height = size != null ? size.height : "150px"
 
-    const getImage = (f) => {
-        switch(f)
-        {
-            case "h":
-                return heart;
-            case "d":
-                return diamond;
-            case "c":
-                return club;
-            case "s":
-                return spade;
-            default: 
-                return null;
-        }
+  const getImage = (f) => {
+    switch(f)
+    {
+      case "h":
+        return heart;
+      case "d":
+        return diamond;
+      case "c":
+        return club;
+      case "s":
+        return spade;
+      default: 
+        return null;
     }
+  }
 
-    const card_style = {
-        width: width,
-        height: height,
-        color: (card.suit === "h" || card.suit === "d") ? "red" : "black",
-        backgroundImage: `url(${getImage(card.suit)})`,
-        backgroundRepeat: "no-repeat",
-        backgroundPosition: "10% 10%",
-        backgroundSize: "20% 15%",
-        borderColor: (active ? 'red' : '#ccc')
-    }
+  const card_style = {
+    width: width,
+    height: height,
+    color: (card.suit === "h" || card.suit === "d") ? "red" : "black",
+    backgroundImage: `url(${getImage(card.suit)})`,
+    backgroundRepeat: "no-repeat",
+    backgroundPosition: "10% 10%",
+    backgroundSize: "20% 15%",
+    borderColor: (active ? 'red' : '#ccc')
+  }
 
-    return (
-        <div className={styles.card + " d-flex align-items-center justify-content-center"} style={card_style} onClick={() => pickup(id)}>
-            <span className={styles.number}>{card.number.toString().toUpperCase()}</span>
-        </div>
-    )
+  return (
+    <div className={styles.card + " d-flex align-items-center justify-content-center"} style={card_style} onClick={() => pickup(id)}>
+      <span className={styles.number}>{card.number.toString().toUpperCase()}</span>
+    </div>
+  )
 }
