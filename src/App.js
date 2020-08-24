c3dd63c5ddac31b075f87867691ffc6dcc8381ff
wjone <engel0088@gmail.com>
2020-08-24 10:29:41 +0900

    refactor: layout

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { connect } from 'react-redux'
-import { Container, Jumbotron, Row, Col, Button, Alert, Modal  } from 'react-bootstrap';
+import { Container, Row, Col, Button, Modal, Footer  } from 'react-bootstrap';
 import CardHolder from "./components/CardHolder"
 
 import { resetCardsStatus, appendScoreHistory, clearScoreHistory } from "./actions/scoreActions"
@@ -69,6 +69,9 @@ const App = ({holdersState, score, gameOver, scoreHistory, resetAllCardsStatus, 
   const [showGameOverModal, setShowGameOverModal] = useState(false)
   const onCloseModal = () => setShowGameOverModal(false)
 
+  const [showHelpModal, setShowHelpModal] = useState(false)
+  const onCloseHelpModal = () => setShowHelpModal(false)
+
   return (
     <Container>
       <Modal show={showGameOverModal} onHide={onCloseModal}>
@@ -82,69 +85,73 @@ const App = ({holdersState, score, gameOver, scoreHistory, resetAllCardsStatus, 
         </Modal.Footer>
       </Modal>
 
+      <Modal show={showHelpModal} onHide={onCloseHelpModal}>
+        <Modal.Header closeButton><Modal.Title>How to play?</Modal.Title></Modal.Header>
+        <Modal.Body>
+          <p className="lead my-0 text-muted">This is a card game by React</p>
+          <p className="lead my-0 text-muted">Please select a pair of cards with same number. Then it will disappear.</p>
+          <p className="lead my-0 text-muted">It will be over if you can not find a pair of cards any more.</p>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="primary" onClick={onCloseHelpModal}>Close</Button>
+        </Modal.Footer>
+      </Modal>
+
       <Row>
-        <Col sm="12" md="10" lg="10">
-          <Jumbotron className="mt-3">
-            <div className="d-flex justify-content-between align-items-center border-bottom border-primary mb-2">
-              <div className="d-flex align-items-center">
-                <a href="https://github.com/yeahCH/playing-card"><img src={githubIcon} alt="github.com/yeahch/playing-card" style={{width: '36px', marginRight: '10px'}}/></a>
-                <h1 className="text-primary">60K Points</h1>
-              </div>
-
-              <div>
-                <Button variant="warning" className="mr-2" onClick={() => onClickResetHistory()}>Reset score</Button>
-                <Button variant="danger" onClick={() => onClickRestart()}>New game</Button>
-              </div>
+        <Col sm="12" md="10" lg="10" className="bg-light mt-2">
+          <div className="d-flex justify-content-between align-items-center border-bottom border-primary mb-2">
+            <div className="d-flex align-items-center">
+              <h1 className="text-primary">60K Points</h1>
             </div>
-            <Row>
-              <Col xs="10">
-                <p className="lead my-0 text-muted">This is a card game by React</p>
-                <p className="lead my-0 text-muted">Please select a pair of cards with same number. Then it will disappear.</p>
-                <p className="lead my-0 text-muted">It will be over if you can not find a pair of cards any more.</p>
-              </Col>
 
-              <Col xs="2">
-                <h1 className="text-danger">{score}</h1>
-              </Col>
-            </Row>
-            {
-              (holdersState.length > 0) && (
-                <div>
-                  <Row>
-                    <CardHolder id={0} key={0}/>
-                    <CardHolder id={1} key={1}/>
-                    <CardHolder id={2} key={2}/>
-                    <CardHolder id={3} key={3}/>
-                    <CardHolder id={4} key={4}/>
-                  </Row>
-                  <Row>
-                    <CardHolder id={10} key={10}/>
-                    <CardHolder id={11} key={11}/>
-                    <CardHolder id={12} key={12}/>
-                    <CardHolder id={13} key={13}/>
-                    <CardHolder id={14} key={14}/>
-                  </Row>
-                  <Row>
-                    <CardHolder id={5} key={5}/>
-                    <CardHolder id={6} key={6}/>
-                    <CardHolder id={7} key={7}/>
-                    <CardHolder id={8} key={8}/>
-                    <CardHolder id={9} key={9}/>
-                  </Row>
-                  <Row>
-                    <Col></Col>
-                    <Col></Col>
-                    <CardHolder id={15} key={15} />
-                    <Col></Col>
-                    <Col></Col>
-                  </Row>
-                </div>
-              )
-            }
-          </Jumbotron>
+            <div className="d-flex">
+              <Button variant="info" className="mr-1" onClick={() => setShowHelpModal(true)}>Help</Button>
+              <Button variant="warning" className="mr-1" onClick={() => onClickResetHistory()}>Reset score</Button>
+              <Button variant="danger" onClick={() => onClickRestart()}>New game</Button>
+            </div>
+          </div>
+          
+          {
+            (holdersState.length > 0) && (
+              <div>
+                <Row>
+                  <CardHolder id={0} key={0}/>
+                  <CardHolder id={1} key={1}/>
+                  <CardHolder id={2} key={2}/>
+                  <CardHolder id={3} key={3}/>
+                  <CardHolder id={4} key={4}/>
+                </Row>
+                <Row>
+                  <CardHolder id={10} key={10}/>
+                  <CardHolder id={11} key={11}/>
+                  <CardHolder id={12} key={12}/>
+                  <CardHolder id={13} key={13}/>
+                  <CardHolder id={14} key={14}/>
+                </Row>
+                <Row>
+                  <CardHolder id={5} key={5}/>
+                  <CardHolder id={6} key={6}/>
+                  <CardHolder id={7} key={7}/>
+                  <CardHolder id={8} key={8}/>
+                  <CardHolder id={9} key={9}/>
+                </Row>
+                <Row>
+                  <Col></Col>
+                  <Col></Col>
+                  <CardHolder id={15} key={15} />
+                  <Col></Col>
+                  <Col></Col>
+                </Row>
+              </div>
+            )
+          }
+        
         </Col>
         <Col sm="12" md="2" lg="2" className="mt-3">
-          <h4 className="text-primary">High Scores</h4>
+          <h4 className="text-info">Score: </h4>
+          <h2 className="text-danger text-center">{score}</h2>
+          <hr/>
+          <h4 className="text-info">High Scores</h4>
           {
             scoreHistory.sort((a, b) => {
               return b.score - a.score
@@ -157,6 +164,17 @@ const App = ({holdersState, score, gameOver, scoreHistory, resetAllCardsStatus, 
           }
         </Col>
       </Row>
+
+      <footer className="border-top pt-3 my-md-5">
+        <Row>
+          <Col className="text-center">
+            <h5>
+              <a href="https://github.com/yeahCH/playing-card"><img src={githubIcon} alt="github.com/yeahch/playing-card" style={{width: '36px', marginRight: '10px'}}/></a>
+              @yeahch : 2020 by React.JS
+            </h5>
+          </Col>
+        </Row>
+      </footer>
     </Container>
   )
 }
